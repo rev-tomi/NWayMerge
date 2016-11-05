d34b0b63cd1fd98eaf7ce6111f335b4b8168f905
Tamas Rev <tamas.rev@gmail.com>
2016-11-05 12:41:00 +0100

    can merge 1 stream too

--- NWayMerge/src/main/java/com/example/nway/Merger.java
+++ NWayMerge/src/main/java/com/example/nway/Merger.java
@@ -1,13 +1,11 @@
 package com.example.nway;
 
-import java.util.Collections;
 import java.util.stream.Stream;
 
 public class Merger {
 
 	public <T extends Comparable<T>> Stream<T> merge(Stream<T>... inputs) {
-		// TODO Auto-generated method stub
-		return Collections.<T>emptyList().stream();
+		return inputs[0];
 	}
 
 }
--- NWayMerge/src/test/java/com/example/nway/MergeTest.java
+++ NWayMerge/src/test/java/com/example/nway/MergeTest.java
@@ -1,11 +1,12 @@
 package com.example.nway;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -22,6 +23,21 @@ public class MergeTest {
 		
 		// THEN
 		List<Integer> collected = merged.collect(Collectors.toList());
-		assertEquals(collected, Collections.<Integer>emptyList());
+		assertEquals(Collections.<Integer>emptyList(), collected);
+	}
+
+	@Test
+	public void testMergeOneStream() {
+		// GIVEN
+		Merger merger = new Merger();
+		List<Integer> inputList = Arrays.asList(1, 2, 3);
+		Stream<Integer> input = inputList.stream();
+		
+		// WHEN
+		Stream<Integer> merged = merger.merge(input);
+		
+		// THEN
+		List<Integer> collected = merged.collect(Collectors.toList());
+		assertEquals(inputList, collected);
 	}
 }
